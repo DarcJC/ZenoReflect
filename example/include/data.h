24eb1dc098457951cc0116a9f757db55658af0ae
DarcJC <me@darc.pro>
2024-07-11 14:05:47 +0800

    Fix string compare Add utilities to provide virtual interface Add field visit example

--- example/include/data.h
+++ example/include/data.h
@@ -2,9 +2,11 @@
 
 #include "reflect/core.hpp"
 #include "reflect/type"
+#include "reflect/reflection_traits.hpp"
 #include <limits>
 #include <string>
 #include <vector>
+#include <iostream>
 #include "reflect/reflection.generated.hpp"
 
 namespace test {
@@ -36,7 +38,9 @@ namespace zeno
         std::string s = "测试内容";
 
         ZMETHOD(Name="做些事") 
-        void DoSomething(int value) const;
+        void DoSomething(int value) const {
+            std::cout << "Doing something " << value << "x ..." << std::endl;
+        }
     };
 
     struct ZRECORD(DisplayName="你好") 基类测试 : public AliasType2 {
@@ -47,11 +51,12 @@ namespace zeno
         const char* 字段1 = "Hello World";
     };
 
-    class ZRECORD(Test="true") Hhhh {
+    class ZRECORD(Test="true") Hhhh : public reflect::TEnableVirtualRefectionInfo<Hhhh> {
     public:
         int a1;
         std::string test;
 
+        zeno::reflect::Any a2;
     };
 }
 
--- example/src/example.cpp
+++ example/src/example.cpp
@@ -1,4 +1,5 @@
 #include "data.h"
+#include "print.h"
 #include "reflect/container/object_proxy"
 #include "reflect/container/any"
 #include "reflect/container/arraylist"
@@ -7,46 +8,12 @@
 
 using namespace zeno::reflect;
 
-void zeno::IAmPrimitve::DoSomething(int value) const {
-    std::cout << "Doing something " << value << "x ..." << std::endl;
-}
-
 namespace zeno {
     std::ostream& operator<<(std::ostream& os, const IAmPrimitve& p) {
         os << "i32 = " << p.i32;
         return os;
     }
 
-namespace reflect {
-
-    std::ostream& operator<<(std::ostream& os, const Any& any) {
-        // 检查any容器是否为空 也可以用any.has_value()
-        if (any) {
-            // 获取类型句柄 这玩意大小只有 aligned(sizeof(size_t) + sizeof(bool), sizeof(size_t)) 可以随意拷贝
-            TypeHandle type_visitor = any.type();
-            // 打印信息
-            os << "===\t" << type_visitor->get_info().canonical_typename.c_str() << "\t===\n";
-            // 获取类型所有字段的访问器
-            const auto& fields =  type_visitor->get_member_fields();
-            // 遍历类型字段
-            for (const auto& field_visitor : fields) {
-                // 打印字段名称
-                os << "\t" << field_visitor->get_name().c_str() << "\t=\t";
-                // 各种类型的输出适配器 这里只实现了int的
-                if (field_visitor->get_field_type() == get_type<int>()) {
-                    os << *field_visitor->get_field_ptr_typed<int>(any) << "\n";
-                }
-                if (field_visitor->get_field_type() == get_type<std::string>()) {
-                    os << *field_visitor->get_field_ptr_typed<std::string>(any) << "\n";
-                }
-            }
-            os << "===========================================";
-        } else {
-            os << "<nullany>";
-        }
-        return os;
-    }
-}
 }
 
 void fix_console_encoding();
