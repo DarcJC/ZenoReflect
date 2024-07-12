f6c33d9ec8cc190bb06db4ed0f29597735a9fa78
DarcJC <me@darc.pro>
2024-07-12 18:08:26 +0800

    Fix type conversion

--- example/src/any_with_ptr.cpp
+++ example/src/any_with_ptr.cpp
@@ -11,9 +11,17 @@ int main() {
     Any soo = make_any<Soo>();
     Any param1 = make_any<int>(123);
 
-    TypeHandle ttt = get_type<Soo>();
+    TypeHandle handle = get_type<Soo>();
 
-    ttt->get_member_functions()[0]->invoke(soo, { param1 });
+    // param1 with type int will automaticly convert to int*
+    handle->get_member_functions()[0]->invoke(soo, { param1 });
+
+    // Create an Any holding a pointer on stack.
+    int a = 666;
+    param1 = make_any<int*>(&a);
+
+    // Call it with int*
+    handle->get_member_functions()[0]->invoke(soo, { param1 });
 
     return 0;
 }
