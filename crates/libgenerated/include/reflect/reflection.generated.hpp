900164177f28d4da9109af3332136a70cd150f63
DarcJC <me@darc.pro>
2024-07-23 19:05:28 +0800

    (metadata-parser) Naming consistency

---
+++ crates/libgenerated/include/reflect/reflection.generated.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "reflect\Reflect-Tests-any_with_ptr\data.h.generated.hpp"
+#include "reflect\Reflect-Tests-any_with_ptr\print.h.generated.hpp"
+#include "reflect\Reflect-Tests-any_with_ptr\test.h.generated.hpp"
+#include "reflect\Reflect-Tests-example\data.h.generated.hpp"
+#include "reflect\Reflect-Tests-example\print.h.generated.hpp"
+#include "reflect\Reflect-Tests-example\test.h.generated.hpp"
+#include "reflect\Reflect-Tests-field_visit\data.h.generated.hpp"
+#include "reflect\Reflect-Tests-field_visit\print.h.generated.hpp"
+#include "reflect\Reflect-Tests-field_visit\test.h.generated.hpp"
