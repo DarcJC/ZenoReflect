d5e24b1b3485bb5ae6035ad97947b29bef3a664b
DarcJC <me@darc.pro>
2024-07-24 17:51:35 +0800

    (serialization) Add Archive container

--- crates/libserialization/src/binary_serialization.cpp
+++ crates/libserialization/src/binary_serialization.cpp
@@ -19,24 +19,6 @@ namespace reflect
 
     using IdentifierType = size_t;
 
-    enum class TypeIdentifier : IdentifierType {
-        Int32 = 1,
-        Float = 2,
-        Double = 3,
-        Char = 4,
-        String = 5,
-        Int8 = 6,
-        Int16 = 7,
-        Int64 = 8,
-        Uint8 = 9,
-        Uint16 = 10,
-        Uint32 = 11,
-        Uint64 = 12,
-        Float32 = 13,
-        Float64 = 14,
-        MaxNum,
-    };
-
     class DefaultBinarySerializer : public ISerializer {
     public:
         ISerializer& serialize(UniquePtr<IWritableStream>& out_stream, const Any& input) override {
@@ -46,30 +28,30 @@ namespace reflect
 
             const RTTITypeInfo& rtti_info = input.type();
 
-            if (rtti_info == type_info<int>()) {
-                serialize_basic_type<int>(out_stream, input, TypeIdentifier::Int32);
+            if (rtti_info == type_info<int32_t>()) {
+                serialize_basic_type<int>(out_stream, input, type_info<int32_t>().hash_code());
             } else if (rtti_info == type_info<float>()) {
-                serialize_basic_type<float>(out_stream, input, TypeIdentifier::Float);
+                serialize_basic_type<float>(out_stream, input, type_info<float>().hash_code());
             } else if (rtti_info == type_info<double>()) {
-                serialize_basic_type<double>(out_stream, input, TypeIdentifier::Double);
+                serialize_basic_type<double>(out_stream, input, type_info<double>().hash_code());
             } else if (rtti_info == type_info<char>()) {
-                serialize_basic_type<char>(out_stream, input, TypeIdentifier::Char);
+                serialize_basic_type<char>(out_stream, input, type_info<char>().hash_code());
             } else if (rtti_info == type_info<std::string>()) {
                 serialize_string(out_stream, input);
             } else if (rtti_info == type_info<int8_t>()) {
-                serialize_basic_type<int8_t>(out_stream, input, TypeIdentifier::Int8);
+                serialize_basic_type<int8_t>(out_stream, input, type_info<int8_t>().hash_code());
             } else if (rtti_info == type_info<int16_t>()) {
-                serialize_basic_type<int16_t>(out_stream, input, TypeIdentifier::Int16);
+                serialize_basic_type<int16_t>(out_stream, input, type_info<int16_t>().hash_code());
             } else if (rtti_info == type_info<int64_t>()) {
-                serialize_basic_type<int64_t>(out_stream, input, TypeIdentifier::Int64);
+                serialize_basic_type<int64_t>(out_stream, input, type_info<int32_t>().hash_code());
             } else if (rtti_info == type_info<uint8_t>()) {
-                serialize_basic_type<uint8_t>(out_stream, input, TypeIdentifier::Uint8);
+                serialize_basic_type<uint8_t>(out_stream, input, type_info<uint8_t>().hash_code());
             } else if (rtti_info == type_info<uint16_t>()) {
-                serialize_basic_type<uint16_t>(out_stream, input, TypeIdentifier::Uint16);
+                serialize_basic_type<uint16_t>(out_stream, input, type_info<uint16_t>().hash_code());
             } else if (rtti_info == type_info<uint32_t>()) {
-                serialize_basic_type<uint32_t>(out_stream, input, TypeIdentifier::Uint32);
+                serialize_basic_type<uint32_t>(out_stream, input, type_info<uint32_t>().hash_code());
             } else if (rtti_info == type_info<uint64_t>()) {
-                serialize_basic_type<uint64_t>(out_stream, input, TypeIdentifier::Uint64);
+                serialize_basic_type<uint64_t>(out_stream, input, type_info<uint64_t>().hash_code());
             } else {
                 throw std::runtime_error("Unsupported type for serialization.");
             }
@@ -83,24 +65,35 @@ namespace reflect
             }
 
             // Read the type identifier
-            IdentifierType identifier = 0;
-            data_stream->read(reinterpret_cast<uint8_t*>(&identifier), sizeof(IdentifierType));
-            TypeIdentifier type_identifier = static_cast<TypeIdentifier>(identifier);
+            IdentifierType type_identifier = 0;
+            data_stream->read(reinterpret_cast<uint8_t*>(&type_identifier), sizeof(IdentifierType));
 
-            switch (type_identifier) {
-                case TypeIdentifier::Int32: deserialize_basic_type<int>(data_stream, output); break;
-                case TypeIdentifier::Float: deserialize_basic_type<float>(data_stream, output); break;
-                case TypeIdentifier::Double: deserialize_basic_type<double>(data_stream, output); break;
-                case TypeIdentifier::Char: deserialize_basic_type<char>(data_stream, output); break;
-                case TypeIdentifier::String: deserialize_string(data_stream, output); break;
-                case TypeIdentifier::Int8: deserialize_basic_type<int8_t>(data_stream, output); break;
-                case TypeIdentifier::Int16: deserialize_basic_type<int16_t>(data_stream, output); break;
-                case TypeIdentifier::Int64: deserialize_basic_type<int64_t>(data_stream, output); break;
-                case TypeIdentifier::Uint8: deserialize_basic_type<uint8_t>(data_stream, output); break;
-                case TypeIdentifier::Uint16: deserialize_basic_type<uint16_t>(data_stream, output); break;
-                case TypeIdentifier::Uint32: deserialize_basic_type<uint32_t>(data_stream, output); break;
-                case TypeIdentifier::Uint64: deserialize_basic_type<uint64_t>(data_stream, output); break;
-                default: throw std::runtime_error("Unsupported type for deserialization.");
+            if (type_identifier == type_info<float>().hash_code()) {
+                deserialize_basic_type<float>(data_stream, output);
+            } else if (type_identifier == type_info<double>().hash_code()) {
+                deserialize_basic_type<double>(data_stream, output);
+            } else if (type_identifier == type_info<char>().hash_code()) {
+                deserialize_basic_type<char>(data_stream, output);
+            } else if (type_identifier == type_info<std::string>().hash_code()) {
+                deserialize_string(data_stream, output);
+            } else if (type_identifier == type_info<int8_t>().hash_code()) {
+                deserialize_basic_type<int8_t>(data_stream, output);
+            } else if (type_identifier == type_info<int16_t>().hash_code()) {
+                deserialize_basic_type<int16_t>(data_stream, output);
+            } else if (type_identifier == type_info<int32_t>().hash_code()) {
+                deserialize_basic_type<int32_t>(data_stream, output);
+            } else if (type_identifier == type_info<int64_t>().hash_code()) {
+                deserialize_basic_type<int64_t>(data_stream, output);
+            } else if (type_identifier == type_info<uint8_t>().hash_code()) {
+                deserialize_basic_type<uint8_t>(data_stream, output);
+            } else if (type_identifier == type_info<uint16_t>().hash_code()) {
+                deserialize_basic_type<uint16_t>(data_stream, output);
+            } else if (type_identifier == type_info<uint32_t>().hash_code()) {
+                deserialize_basic_type<uint32_t>(data_stream, output);
+            } else if (type_identifier == type_info<uint64_t>().hash_code()) {
+                deserialize_basic_type<uint64_t>(data_stream, output);
+            } else {
+                throw std::runtime_error("Unsupported type for deserialization.");
             }
 
             return *this;
@@ -108,35 +101,42 @@ namespace reflect
 
     private:
         template <typename T>
-        void serialize_basic_type(UniquePtr<IWritableStream>& out_stream, const Any& input, TypeIdentifier type_identifier) {
+        bool serialize_basic_type(UniquePtr<IWritableStream>& out_stream, const Any& input, IdentifierType type_identifier) {
             T value = any_cast<T>(input);
-            IdentifierType identifier = IdentifierType(type_identifier);
-            out_stream->write(reinterpret_cast<const uint8_t*>(&identifier), sizeof(IdentifierType));
+            out_stream->write(reinterpret_cast<const uint8_t*>(&type_identifier), sizeof(IdentifierType));
             out_stream->write(reinterpret_cast<const uint8_t*>(&value), sizeof(T));
+
+            return true;
         }
 
         template <typename T>
-        void deserialize_basic_type(const UniquePtr<IReadableStream>& data_stream, Any& output) {
+        bool deserialize_basic_type(const UniquePtr<IReadableStream>& data_stream, Any& output) {
             T value;
             data_stream->read(reinterpret_cast<uint8_t*>(&value), sizeof(T));
             output = Any(value);
+
+            return true;
         }
 
-        void serialize_string(UniquePtr<IWritableStream>& out_stream, const Any& input) {
+        bool serialize_string(UniquePtr<IWritableStream>& out_stream, const Any& input) {
             std::string& value = any_cast<std::string&>(input);
-            IdentifierType identifier = static_cast<IdentifierType>(TypeIdentifier::String);
+            IdentifierType identifier = type_info<std::string>().hash_code();
             out_stream->write(reinterpret_cast<const uint8_t*>(&identifier), sizeof(IdentifierType));
             uint32_t length = static_cast<uint32_t>(value.size());
             out_stream->write(reinterpret_cast<const uint8_t*>(&length), sizeof(length));
             out_stream->write(reinterpret_cast<const uint8_t*>(value.data()), length);
+
+            return true;
         }
 
-        void deserialize_string(const UniquePtr<IReadableStream>& data_stream, Any& output) {
+        bool deserialize_string(const UniquePtr<IReadableStream>& data_stream, Any& output) {
             uint32_t length;
             data_stream->read(reinterpret_cast<uint8_t*>(&length), sizeof(length));
             std::string value(length, '\0');
-            data_stream->read(reinterpret_cast<uint8_t*>(&value[0]), length);
+            data_stream->read(reinterpret_cast<uint8_t*>(value.data()), length);
             output = Any(value);
+
+            return true;
         }
     };
 
