06ca4a2a4f98326db2d23478b12fe8d6c43b0a14
DarcJC <me@darc.pro>
2024-07-24 21:02:22 +0800

    (serialization) Uncomment previous codes

--- crates/libserialization/tests/test.cpp
+++ crates/libserialization/tests/test.cpp
@@ -12,29 +12,29 @@ int main() {
 
     using namespace zeno::reflect;
 
-    // // Create a serializer
-    // auto serializer = create_default_serializer();
+    // Create a serializer
+    auto serializer = create_default_serializer();
 
-    // // Example data
-    // std::string example_string = "Hello, World!";
-    // Any example_any = example_string;
+    // Example data
+    std::string example_string = "Hello, World!";
+    Any example_any = example_string;
 
-    // // Serialize to a file
-    // {
-    //     UniquePtr<IWritableStream> writable_stream = create_file_write_stream("example.zarchive");
-    //     serializer->serialize(writable_stream, example_any);
-    // }
+    // Serialize to a file
+    {
+        UniquePtr<IWritableStream> writable_stream = create_file_write_stream("example.zarchive");
+        serializer->serialize(writable_stream, example_any);
+    }
 
-    // // Deserialize from a file
-    // if (1)
-    // {
-    //     UniquePtr<IReadableStream> readable_stream = create_file_read_stream("example.zarchive");
-    //     Any deserialized_any;
-    //     serializer->deserialize(readable_stream, deserialized_any);
+    // Deserialize from a file
+    if (1)
+    {
+        UniquePtr<IReadableStream> readable_stream = create_file_read_stream("example.zarchive");
+        Any deserialized_any;
+        serializer->deserialize(readable_stream, deserialized_any);
 
-    //     std::string deserialized_string = any_cast<std::string>(deserialized_any);
-    //     std::cout << "Deserialized value: " << deserialized_string << std::endl;
-    // }
+        std::string deserialized_string = any_cast<std::string>(deserialized_any);
+        std::cout << "Deserialized value: " << deserialized_string << std::endl;
+    }
 
     // How are you?
     {
