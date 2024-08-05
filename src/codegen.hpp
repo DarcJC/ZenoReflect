DarcJC/ZenoReflect

commit 04f0cbd1c34a6251efc91498dbd428374c59127b

[fix] const issues of generated type info.

src/codegen.hpp
@@ -54,11 +54,10 @@ namespace zeno::reflect
         RTTITypeGenerator(
             clang::QualType qual_type
         ) : m_qual_type(qual_type) {
-            m_qual_type = m_qual_type->getCanonicalTypeUnqualified();
         }
 
-        std::string compile(CodeCompilerState& state, const std::string& dispName = "") {
-            const size_t hash_value = HashImpl{}(m_qual_type.getCanonicalType().getAsString());
+        std::string compile(CodeCompilerState& state, const std::string& dispName = "", bool hasConstMark = false) {
+            const size_t hash_value = HashImpl{}(m_qual_type.getAsString());
             std::string cppType = m_qual_type.getAsString();
             std::string name = m_qual_type.getCanonicalType().getAsString();
             if (cppType.find("_Bool") != std::string::npos) {
@@ -89,7 +88,9 @@ namespace zeno::reflect
             data["isRValueRef"] = m_qual_type->isRValueReferenceType();
             data["isLValueRef"] = m_qual_type->isLValueReferenceType();
             data["isConst"] = m_qual_type.isConstQualified();
-            
+            if (hasConstMark) {
+                data["isConst"] = true;
+            }
             return inja::render(text::RTTI, data);
         }
 
@@ -118,11 +119,12 @@ namespace zeno::reflect
         }
 
         template <ICodeCompiler T = RTTITypeGenerator<>>
-        void add_rtti_type(clang::QualType type, const std::string& dispName = "") {
-            if (type.getAsString() == "void" || type.getAsString() == "void *" || type.getAsString() == "void &" || type.getAsString() == "const void &" || type.getAsString() == "void &&" || type.getAsString() == "std::nullptr_t") {
+        void add_rtti_type(clang::QualType type, const std::string& dispName = "", bool bHasConstMark = false) {
+            const auto& typeStr = type.getAsString();
+            if (typeStr == "void" || typeStr == "void *" || typeStr == "void &" || typeStr == "const void &" || typeStr == "void &&" || typeStr == "std::nullptr_t") {
                 return;
             }
-            m_rtti_block << RTTITypeGenerator(type).compile(m_compiler_state, dispName);
+            m_rtti_block << RTTITypeGenerator(type).compile(m_compiler_state, dispName, bHasConstMark);
         }
     };
 }

src/parser.cpp
@@ -19,7 +19,9 @@ inline void add_type_to_generator(T* context, clang::QualType type, const std::s
     auto& gen = context->template_header_generator;
     auto& scoped_ctx = context->scoped_context;
 
-    auto add_rtti_type = [&](clang::QualType qt, const std::string& dispName = "") { gen->add_rtti_type(qt, dispName); };
+    auto add_rtti_type = [&](clang::QualType qt, const std::string& dispName = "", bool hasConstMark = false) { 
+        gen->add_rtti_type(qt, dispName, hasConstMark);
+    };
 
     add_rtti_type(type, dispName);
 
@@ -30,7 +32,7 @@ inline void add_type_to_generator(T* context, clang::QualType type, const std::s
     add_rtti_type(scoped_ctx->getConstType(canonical_type), dispName);
     add_rtti_type(scoped_ctx->getLValueReferenceType(canonical_type), dispName);
     add_rtti_type(scoped_ctx->getRValueReferenceType(canonical_type), dispName);
-    add_rtti_type(scoped_ctx->getLValueReferenceType(scoped_ctx->getConstType(canonical_type)), dispName);
+    add_rtti_type(scoped_ctx->getLValueReferenceType(scoped_ctx->getConstType(canonical_type)), dispName, true);
     add_rtti_type(scoped_ctx->getPointerType(scoped_ctx->getConstType(canonical_type)), dispName);
     add_rtti_type(scoped_ctx->getPointerType(canonical_type), dispName);
 }

commit d8e97406e4304b1717d12e040b853770a9715c10

(revert) Fix regression caused by 04f0cbd. (#15)

src/codegen.hpp
@@ -54,6 +54,7 @@ namespace zeno::reflect
         RTTITypeGenerator(
             clang::QualType qual_type
         ) : m_qual_type(qual_type) {
+            m_qual_type = m_qual_type->getCanonicalTypeUnqualified();
         }
 
         std::string compile(CodeCompilerState& state, const std::string& dispName = "", bool hasConstMark = false) {